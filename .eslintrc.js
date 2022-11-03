2289ce14671c82c77cb60f4b47abafc546106561
hxpdong <dongstt1096@gmail.com>
2022-11-03 09:51:53 +0700

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es2022: true,
+    },
+    extends: 'eslint:recommended',
+    rules: {
+        'no-unused-vars': 'off'
+    },
+};
