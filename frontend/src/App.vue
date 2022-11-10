691aba460575ac4142c4d531782c5777ad167c28
ndkhangvl <nguyenduykhang410@gmail.com>
2022-11-10 13:06:50 +0700

    Add front end

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style>
+</style>
---
+++ frontend/src/view/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mx-auto w-7/12 my-16"></div>
+</template>
+
+<script>
+
+</script>
